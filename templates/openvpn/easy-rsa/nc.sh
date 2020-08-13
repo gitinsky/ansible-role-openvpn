9205746109d4230a6d5e446fbc220e5c5c7d0b6e
Roman Belyakovsky <ihryamzik@gmail.com>
2020-08-13 05:42:32 +0300

    Revert "improved options"

--- templates/openvpn/easy-rsa/nc.sh
+++ templates/openvpn/easy-rsa/nc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname "$0"`
-if mkdir $1
+if mkdir -v $1
     then
     . ./vars
     ./pkitool $1
@@ -9,46 +9,19 @@ else
     a=1
 fi
 
-# cp keys/$1.crt $1/client.crt
-# cp keys/$1.key $1/client.key
-# cp keys/ca.crt $1/ca.crt
-# cp keys/ta.key $1/
-# cp client.ovpn $1/
-
+cp -v keys/$1.crt $1/client.crt
+cp -v keys/$1.key $1/client.key
+cp -v keys/ca.crt $1/ca.crt
+cp -v keys/ta.key $1/
+cp -v client.ovpn $1/
 pushd $1
-# zip -9 $1.zip client.crt client.key ca.crt ta.key client.ovpn
-# tar cvf - client.crt client.key ca.crt ta.key client.ovpn | gzip -9 - > $1.tgz
+zip -9 $1.zip client.crt client.key ca.crt ta.key client.ovpn
+tar cvf - client.crt client.key ca.crt ta.key client.ovpn | gzip -9 - > $1.tgz
 
-cat <<EOF> {{ openvpn_name }}.ovpn
-client
-dev {{ openvpn_dev }}
-proto udp
-remote {{ openvpn_access_address }} {{ openvpn_listen_port }}
-resolv-retry infinite
-nobind
-;user nobody
-;group nogroup
-persist-key
-persist-tun
-ns-cert-type server
-;tls-auth ta.key 1
-<ca>
-$(cat ../keys/ca.crt)
-</ca>
-<cert>
-$(cat ../keys/$1.crt)
-</cert>
-<key>
-$(cat ../keys/$1.key)
-</key>
-comp-lzo
-verb 3
-EOF
-
-# mkdir -vp share
-#
-# zip -9 -e -P $(openssl rand -base64 32|tee /dev/stderr) share/$1.zip {{ openvpn_name }}.ovpn
-
-# cd share
-# python -m SimpleHTTPServer 8001
+#(
+#cat message.txt
+#printf "\n\n"
+#uuencode $1.zip $1.zip
+#)|mail -s "VPN key for user $1" ihryamzik@gmail.com $1@gmail.com
+python -m SimpleHTTPServer 8001
 popd
